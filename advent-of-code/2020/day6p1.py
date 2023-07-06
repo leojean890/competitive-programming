64c9e3c8b9df256dabfda4415cf1e3256e1efd00
leojean890 <42359699+leojean890@users.noreply.github.com>
2023-07-07 01:25:39 +0200

    Create day6p1.py

---
+++ advent-of-code/2020/day6p1.py
@@ -0,0 +1,13 @@
+
+keys = set()
+counter = 0
+cc = 0
+for i in range(2176):
+    l = input()
+    if not l:
+        counter += len(keys)
+        keys = set()
+    keys |= set(l)
+
+counter += len(keys)
+print(counter) # 6530
