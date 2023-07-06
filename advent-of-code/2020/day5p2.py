969fe00e62c9ee37d0023e59b807f901471bd2dc
leojean890 <42359699+leojean890@users.noreply.github.com>
2023-07-07 01:07:30 +0200

    Create day5p2.py

---
+++ advent-of-code/2020/day5p2.py
@@ -0,0 +1,39 @@
+
+m = 0
+ids = []
+for i in range(789):
+    placement = input()
+    l, r = 0, 127
+    current = 0
+    while placement[current] in "FB":
+        mid = (r + l)
+        if placement[current] == "F":
+            r = mid//2 if mid%2 else mid//2 - 1
+            row = r
+        else:
+            l = mid//2 + 1 if mid%2 else mid//2
+            row = l
+        current += 1
+
+    l, r = 0, 8
+    while current < len(placement):
+        mid = (r + l)
+        if placement[current] == "L":
+            r = mid//2 if mid%2 else mid//2 - 1
+            col = r
+        else:
+            l = mid//2 + 1 if mid%2 else mid//2
+            col = l
+        current += 1
+
+    ind = row * 8 + col
+    ids.append(ind)
+    if ind > m:
+        m = ind
+print(m) # 864
+
+ids.sort()
+
+for i in range(len(ids)-1):
+    if ids[i] + 2 == ids[i+1]:
+        print(ids[i]+1) #739
