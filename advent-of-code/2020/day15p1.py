83f209466f33d7a73a14e6f7b708c4ea3cdf3063
leojean890 <42359699+leojean890@users.noreply.github.com>
2023-07-18 00:00:35 +0200

    Create day15p1.py

---
+++ advent-of-code/2020/day15p1.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+indexes = defaultdict(list)
+tab = [int(i) for i in input().split(",")]
+for i in range(len(tab)):
+    indexes[tab[i]].append(i)
+L = len(tab)
+for i in range(L, 2020):
+    elt = tab[-1]
+    if len(indexes[elt]) == 1:
+        tab.append(0)
+        indexes[tab[i]].append(i)
+    else:
+        tab.append(indexes[elt][-1] - indexes[elt][-2])
+        indexes[tab[i]].append(i)
+
+print(tab[-1]) # 371
