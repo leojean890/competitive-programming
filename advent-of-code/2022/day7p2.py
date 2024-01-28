22d18cfb433f35217c6103d7c07ecce9e252d6d0
leojean890 <42359699+leojean890@users.noreply.github.com>
2024-01-28 11:32:59 +0100

    Update day7p2.py

--- advent-of-code/2022/day7p2.py
+++ advent-of-code/2022/day7p2.py
@@ -1,4 +1,46 @@
+
 from collections import defaultdict
+parents = {}
+currentParent = None
+sums = defaultdict(int)
+children = defaultdict(list)
+visited = set()
+
+while 1:
+    try:
+        a = input().split()
+        if "cd" == a[1]  :
+            if ".." == a[2]:
+                currentParent = parents[currentParent]
+            else:
+                if currentParent:
+                    currentParent = "/".join([currentParent, a[-1]])
+                else:
+                    currentParent = a[-1]
+        elif a[0].isdigit():
+            if (a[0], a[1]) not in visited:
+                sums[currentParent] += int(a[0])
+                visited.add((a[0], a[1]))
+                current = currentParent
+                while current != "/":
+                    current = parents[current]
+                    sums[current] += int(a[0])
+        elif a[0] == "dir":
+            parents["/".join([currentParent, a[-1]])] = currentParent
+            children[currentParent].append("/".join([currentParent, a[-1]]))
+    except Exception as ex:
+        total = 0
+        for x, y in sums.items():
+            if y <= 100000:
+                total += y
+        print(total)
+        exit()
+
+
+
+
+
+"""from collections import defaultdict
 parents = {}
 currentParent = None
 sums = defaultdict(int)
@@ -34,5 +76,5 @@ while 1:
                 if y <= 100000:
                     total += y
             print(total)
-            exit()
+            exit()"""
 
