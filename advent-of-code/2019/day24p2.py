43843be7b9772dc9a10dab713a84a6741fc4c212
leojean890 <42359699+leojean890@users.noreply.github.com>
2024-02-03 07:02:08 +0100

    Create day24p2.py

---
+++ advent-of-code/2019/day24p2.py
@@ -0,0 +1,93 @@
+#d24p2 todo
+from copy import deepcopy
+
+lines = list()
+for i in range(5):
+    lines += [list(input())]
+#print(lines)
+
+emptyLines = list()
+for i in range(5):
+    emptyLines += [['.', '.', '.', '.', '.']]
+#print(emptyLines)
+
+
+def dfs(depth, current):
+    if depth == 201:
+        return 0,[]
+
+    nlines = []
+    counter = 0
+
+    for i in range(5):
+        line = []
+        for j in range(5):
+            if i != 2 or j != 2:
+                neighs = 0
+                if j > 0 and (i,j-1) != (2,2) and current[i][j - 1] == "#":
+                    neighs += 1
+                if i > 0 and (i-1,j) != (2,2) and current[i - 1][j] == "#":
+                    neighs += 1
+                if j < 4 and (i,j+1) != (2,2) and current[i][j + 1] == "#":
+                    neighs += 1
+                if i < 4 and (i+1,j) != (2,2) and current[i + 1][j] == "#":
+                    neighs += 1
+
+                if (i,j-1) == (2,2):
+                    neighs += getNeighs(i,j-1,depth+1)
+                if (i-1,j) == (2,2):
+                    neighs += getNeighs(i-1,j,depth+1)
+                if (i,j+1) == (2,2):
+                    neighs += getNeighs(i,j+1,depth+1)
+                if (i+1,j) == (2,2):
+                    neighs += getNeighs(i+1,j,depth+1)
+                    
+                if i == 0:    
+                    neighs += getNeighs(i-1,j,depth-1)
+                if j == 0:    
+                    neighs += getNeighs(i,j-1,depth-1)
+                if i == 4:    
+                    neighs += getNeighs(i+1,j,depth-1)
+                if j == 4:    
+                    neighs += getNeighs(i,j+1,depth-1)
+                    
+                if current[i][j] == "#":
+                    if neighs == 1:
+                        counter += 1
+                    line.append("#" if neighs == 1 else ".")
+                else:
+                    line.append("#" if neighs in (1, 2) else ".")
+                    if neighs in (1, 2):
+                        counter += 1
+            else:
+                c, nl = dfs(depth + 1, current[2][2])
+                line.append(nl)
+                counter += c
+
+        nlines.append(line)
+    print(depth,counter)
+    return counter, nlines
+
+
+current = emptyLines
+
+for iteration in range(201):
+
+    lines1 = list()
+    for i in range(5):
+        toFill = lines if iteration == 100 else emptyLines
+        lines1.append(toFill[i] if i != 2 else toFill[i][:2] + [deepcopy(current)] + toFill[i][3:])
+
+    current = lines1
+
+    #if iteration in (100,101):
+    #    print(current)
+
+#print(lines1)
+
+wholeCurrent = current
+
+for iteration in range(10): # 200
+    counter, wholeCurrent = dfs(0, wholeCurrent)
+    print(iteration, counter)
+
