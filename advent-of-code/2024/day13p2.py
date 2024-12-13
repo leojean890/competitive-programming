8907fe74d4ae470613b7ddce93c7cf648e548687
leojean890 <42359699+leojean890@users.noreply.github.com>
2024-12-13 12:10:01 +0100

    Create day13p2.py

---
+++ advent-of-code/2024/day13p2.py
@@ -0,0 +1,49 @@
+import sys
+from collections import defaultdict
+from time import process_time
+
+"""
+a * x1 + b * y1 = z1 
+a * x2 + b * x2 = z2
+"""
+
+
+def solve_system(x1, x2, y1, y2, z1, z2):
+    det_A = x1 * y2 - x2 * y1
+    if det_A == 0:
+        return 0
+
+    det_A_a = z1 * y2 - z2 * y1
+    det_A_b = x1 * z2 - x2 * z1
+
+    if det_A_a % det_A != 0 or det_A_b % det_A != 0:
+        return 0
+
+    a = det_A_a // det_A
+    b = det_A_b // det_A
+
+    return 3*a+b
+
+
+sys.setrecursionlimit(100000000)
+start_time = process_time()
+elts = defaultdict(list)
+N = 1279
+penalties = [3,1]
+
+for j in range(N):
+    s = input()
+    if j%4 < 2:
+        elts[j//4].append([int(a[-2:]) for a in s.split(":")[1].split(",")])
+    elif j%4 == 2:
+        elts[j//4].append([10000000000000+int(a.split("=")[1]) for a in s.split(":")[1].split(",")])
+
+score = 0
+
+for index, elt in elts.items():
+    memo = {}
+    points = solve_system(elt[0][0], elt[0][1], elt[1][0], elt[1][1], elt[2][0], elt[2][1])
+    if points < sys.maxsize:
+        score += points
+
+print(score, process_time() - start_time)  # 73458657399094 0.015
